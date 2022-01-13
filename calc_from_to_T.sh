e3f809eea4b1fc6928a1b0e5ae5bbdc00b0b742a
SheridanPO <72259200+SheridanPO@users.noreply.github.com>
2022-01-13 11:52:35 +0000

    Update calc_from_to_T.sh

--- calc_from_to_T.sh
+++ calc_from_to_T.sh
@@ -13,3 +13,4 @@ awk '{a[$2] += $3} END{for (i in a) print i, a[i]}' Transfers_file|sort|grep -v 
 join Transfer_from Transfer_to > From_to_T.txt
 sed -i 's/ /\t/g' From_to_T.txt
 sed -i '1s/^/Branch	From	To\n/' From_to_T.txt
+rm Transfer_from Transfer_to Transfers_file
